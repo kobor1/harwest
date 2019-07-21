a59e6a68f1113445ef6bad01a27a7c335315ae53
Krzysztof Boryczka <boryczkakrzysztof@gmail.com>
2019-07-21 19:20:00 +0200

    Add solution for problem `D - Water Tree` Link: https://codeforces.com/contest/343/problem/D Tags: data structures, dfs and similar, graphs, trees, *2100 Ref: https://codeforces.com/contest/343/submission/57469439

--- codeforces/343/D.cpp
+++ codeforces/343/D.cpp
@@ -24,77 +24,76 @@ const ll INF=0x3f3f3f3f3f3f3f3f;
 const int N=5e5+5;
 const int M=1<<19;
 
-int n, tim;
 vi G[N];
-int sz[N], par[N], in[N], out[N], nxt[N];
-int tree[M*2+5][2];
+int in[N], out[N], tree[M*2+5][2], prop[M*2+5][2];
+int n, tim;
 
-void dfs_sz(int v=1, int p=1){
-	sz[v]=1;
-	par[v]=p;
-	for(auto &u: G[v]){
-		if(u==p) continue;
-		dfs_sz(u, v);
-		sz[v]+=sz[u];
-		if(G[v][0]==par[v] || sz[u]>sz[G[v][0]]) swap(u, G[v][0]);
-	}
-}
-
-void dfs_hld(int v=1){
+void dfs(int v){
 	in[v]=++tim;
-	for(auto &u: G[v]){
-		if(u==par[v]) continue;
-		nxt[u]=(u==G[v][0] ? nxt[v] : u);
-		dfs_hld(u);
+	for(auto &x: G[v]){
+		if(!in[x]) dfs(x);
 	}
 	out[v]=tim;
 }
 
-void add(int l, int r, int val, int op){
-	l+=M-1; r+=M-1;
-	while(l<=r){
-		if(l&1) tree[l++][op]=val;
-		if(!(r&1)) tree[r--][op]=val;
-		l>>=1; r>>=1;
+void push(int v){
+	FOR(i, 0, 1){
+		tree[v*2][i]=max(tree[v*2][i], prop[v][i]);
+		tree[v*2+1][i]=max(tree[v*2+1][i], prop[v][i]);
+		prop[v*2][i]=max(prop[v*2][i], prop[v][i]);
+		prop[v*2+1][i]=max(prop[v*2+1][i], prop[v][i]);
 	}
 }
 
-int query(int l, int op){
-	l+=M-1;
-	int ret=0;
-	while(l){
-		ret=max(ret, tree[l][op]);
-		l>>=1;
+void add(int v, int l, int r, int ll, int rr, int val, int op){
+	if(l>rr || r<ll) return;
+	if(l>=ll && r<=rr){
+		tree[v][op]=val;
+		prop[v][op]=val;
+		return;
 	}
-	return ret;
+	int mid=(l+r)/2;
+	push(v);
+	add(v*2, l, mid, ll, rr, val, op);
+	add(v*2+1, mid+1, r, ll, rr, val, op);
+	FOR(i, 0, 1) tree[v][i]=max({tree[v][i], tree[v*2][i], tree[v*2+1][i]});
 }
 
-void path(int v, int u, int val, int op){
-	while(nxt[v]!=nxt[u]){
-		if(in[nxt[v]]>in[nxt[u]]) swap(v, u);
-		add(in[nxt[u]], in[u], val, op);
-		u=par[nxt[u]];
-	}
-	if(in[v]>in[u]) swap(v, u);
-	add(in[v], in[u], val, op);
+ii query(int v, int l, int r, int ll, int rr){
+	if(l>rr || r<ll) return {0, 0};
+	if(l>=ll && r<=rr) return {tree[v][0], tree[v][1]};
+	int mid=(l+r)/2;
+	push(v);
+	ii ret1=query(v*2, l, mid, ll, rr);
+	ii ret2=query(v*2+1, mid+1, r, ll, rr);
+	FOR(i, 0, 1) tree[v][i]=max({tree[v][i], tree[v*2][i], tree[v*2+1][i]});
+	return {max(ret1.st, ret2.st), max(ret1.nd, ret2.nd)};
 }
 
 void solve(){
-	int a, b, q;
+	int a, b, q, c, v;
+	ii ans1, ans2;
 	cin>>n;
 	FOR(i, 2, n){
 		cin>>a>>b;
 		G[a].pb(b);
 		G[b].pb(a);
 	}
-	dfs_sz();
-	dfs_hld();
+	dfs(1);
 	cin>>q;
 	FOR(i, 1, q){
-		cin>>a>>b;
-		if(a==1) add(in[b], out[b], i, 1);
-		if(a==2) path(1, b, i, 0);
-		if(a==3) cout<<(query(in[b], 0)<query(in[b], 1))<<ent;
+		cin>>c>>v;
+		if(c==1){
+			add(1, 1, M-1, in[v], out[v], i, 0);
+		}
+		if(c==2){
+			add(1, 1, M-1, in[v], in[v], i, 1);
+		}
+		if(c==3){
+			ans1=query(1, 1, M-1, in[v], in[v]);
+			ans2=query(1, 1, M-1, in[v], out[v]);
+			cout<<(ans1.st>ans2.nd)<<ent;
+		}
 	}
 }
 
